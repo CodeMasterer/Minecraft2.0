43a76434e18f58c538929ff7644ad7c9a84ce091
CodeMasterer <91100720+CodeMasterer@users.noreply.github.com>
2022-04-11 21:36:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-'''
-Disclaimer: This solution is not scalable for creating a big world.
-Creating a game like Minecraft requires specialized knowledge and is not as easy
-to make as it looks.
-You'll have to do some sort of chunking of the world and generate a combined mesh
-instead of separate blocks if you want it to run fast. You can use the Mesh class for this.
-You can then use blocks with colliders like in this example in a small area
-around the player so you can interact with the world.
-'''
-
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
